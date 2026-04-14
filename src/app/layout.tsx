76f5e310cf9c80abc8372f59a1431bf4a5349982
Renan Galhardo <rgalhardo07@gmail.com>
2026-04-14 16:24:53 -0300

    feat: adicionar Google Tag Manager (G-5QQP3TLG3G)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,6 +113,24 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtag-js"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5QQP3TLG3G"
+        />
+        <Script
+          id="gtag-config"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5QQP3TLG3G');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
